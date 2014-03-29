92b875227d6167dd5c52a26f49341e12091cd8fe
max <max@localhost>
2014-03-29 16:00:09 +0100

    versione iniziale vuota

---
+++ main.c
@@ -0,0 +1,14 @@
+
+/*			GESTIONE SENSORE DI COLORE e TERMOMETRO INFRARED		*/
+
+
+#include <msp430.h> 
+
+/*
+ * main.c
+ */
+int main(void) {
+    WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
+	
+	return 0;
+}
